7492c15e2c4fb645997c7a779b1d49f07f607c3b
Alexfordevops <matheusalexandre.in@gmail.com>
2025-11-01 17:38:32 -0300

    Feat: dashboard-page with selectors

--- services/web-app/src/app/pages/dashboard-page/dashboard-page.ts
+++ services/web-app/src/app/pages/dashboard-page/dashboard-page.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import {NavbarLayout} from '../../layouts/navbar-layout/navbar-layout';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-page',
   imports: [
-    NavbarLayout
+    NavbarLayout,
+    CommonModule
+
   ],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.css',
 })
 export class DashboardPage {
+
+  activeTab: string = 'my-balance';
+
+  setActiveTab(tab: string): void {
+    this.activeTab = tab;
+  }
 
 }
